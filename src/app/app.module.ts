7ee47c8b2800b457289c488b6c9066225bdaf283
gabyto19 <108758131+gabyto19@users.noreply.github.com>
2023-06-02 17:14:45 +0400

    QAlab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SignModalComponent } from './sign-modal/sign-modal.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import { RegisterModalComponent } from './register-modal/register-modal.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    SignModalComponent,
+    WelcomePageComponent,
+    RegisterModalComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign-modal/sign-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-modal',
+  templateUrl: './sign-modal.component.html',
+  styleUrls: ['./sign-modal.component.css']
+})
+export class SignModalComponent {
+
+}
